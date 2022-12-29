95eb7d242eb71ca7c44e2be9f2bd13d2216757bd
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-12-29 11:47:58 +0100

    configuration with yaml added

--- src/main/java/com/vendas/Configuration/MyConfiguration.java
+++ src/main/java/com/vendas/Configuration/MyConfiguration.java
@@ -1,14 +1,12 @@
 package com.vendas.Configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MyConfiguration {
-
-    @Bean( name = "my_bean")
-    public String configurationMethod() {
-        return "This is a method for configuration";
-    }
-    
+	
+	@Bean( name = "my_configuration")
+	public String myConfiguration() {
+		return "This is how do you configure your app.";
+	}
 }
